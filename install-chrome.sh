2148190f013a33bc025ba25c9517855b25a52210
Reynero <reynero96@gmail.com>
2024-10-15 20:12:37 -0400

    2nd commit

---
+++ install-chrome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Download Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install Chrome
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+
+echo "Chrome installation completed."
